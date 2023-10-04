a2fa139f462d9339fbc8d5fb0e5c017a197977e9
Md. Arshad <arshadchowdhury46@gmail.com>
2023-10-05 01:08:24 +0600

    Done setting up the project with React, Vite and MUI

---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import SignUp from "./components/SignUp";
+
+import "./index.css";
+import App from "./App";
+import SignInSide from "./components/SignIn";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/sign-in",
+    element: <SignInSide />,
+  },
+  {
+    path: "/sign-up",
+    element: <SignUp />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
